fd78723a54785576708fa9859c705c5950c0ff90
tyru <tyru.exe@gmail.com>
2010-08-18 22:07:16 +0900

    users is not necessary.
    So what's the next?

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 
-from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
